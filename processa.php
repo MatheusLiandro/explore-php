fb54059fb55a5b00278f9a38b464582b6dbd02b7
Liandro <mateuzinho1@live.com>
2022-10-26 11:55:22 -0300

    update

--- processa.php
+++ processa.php
@@ -1,6 +1,6 @@
 <?php
 
-//Recebe os campos do formulario
+//Recebe os campos do formulário
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 $sexo = $_POST['sexo'];
@@ -11,44 +11,42 @@ $estado = $_POST['estado'];
 $cidade = $_POST['cidade'];
 $destinos = $_POST['destinos'];
 $hospedagem = $_POST['hospedagem'];
-$mensagem = $_POST['menssagem'];
-$dt_cadastro = date('Y-m-d');
+$mensagem = $_POST['mensagem'];
+$dt_cadastro = $_POST['dt-cadastro'];
 
-//para investigar variaveis e expressões
-
-// var_dump($_POST);
+//para investigar variáveis e expressões
+//var_dump($_POST);
 
 //CONECTA AO BANCO E GRAVA OS DADOS (insert com PDO)
 try {
-    //tenta fazer algo
-
-    //instancia o banco por meio do pdo
+    //instancia o banco por meio do PDO
     $pdo = new PDO('mysql:host=localhost;dbname=explore', 'root', '');
-    //INSERT na tabela  de users
-    $sql = $pdo->prepare('INSERT into users(
-        nome, email, sexo, telefone, senha, idade, estado, cidade, destinos, hospedagem,
-        mensagem, dt_cadastro)
-        values(:nome, :email, :sexo, :telefone, :senha, :idade, :estado, :cidade, :destinos, :hospedagem,
-        :mensagem, :dt_cadastro)');
+    //INSERT na tabela users
+    $sql = $pdo->prepare('INSERT into users 
+    (nome, email, sexo, telefone, senha, idade, estado, cidade, 
+    destinos, hospedagem, mensagem, dt_cadastro)
+    values(:nome, :email, :sexo, :telefone, :senha, :idade, :estado, :cidade, 
+    :destinos, :hospedagem, :mensagem, :dt_cadastro)');
     $sql->execute(array(
         ':nome' => $nome,
-        ':email' => $email,
-        ':sexo' => $sexo,
-        ':telefone' => $telefone,
-        ':senha' => md5($senha),
-        ':idade' => $idade,
-        ':estado' => $estado,
-        ':cidade' => $cidade,
-        ':destinos' => $destinos,
-        ':hospedagem' => $hospedagem,
-        ':mensagem' => $mensagem,
-        ':dt_cadastro' => $dt_cadastro
-
+        ':email' => $email, 
+        ':sexo' => $sexo, 
+        ':telefone' => $telefone, 
+        ':senha'=> md5($senha), 
+        ':idade'=> $idade, 
+        ':estado'=> $estado, 
+        ':cidade'=> $cidade,
+        ':destinos'=> implode(',', $destinos), //converte array em texto 
+        ':hospedagem'=> $hospedagem, 
+        ':mensagem'=> $mensagem, 
+        ':dt_cadastro'=> date('Y-m-d', strtotime($dt_cadastro))
     ));
 
-    echo '<h1>Usuário cadastrado</h1>';
-    var_dump($_POST);
+    //carrega a página index.html enviando variável GET cadastro
+    header('Location: index.html?cadastro=ok');   
+    
+
 } catch (PDOException $erro) {
     //se der erro, exibe o erro aqui
     echo $erro;
-}
+} 
